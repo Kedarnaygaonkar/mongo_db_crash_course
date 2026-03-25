7343dc5a6d4afbfc6c3ccebacbe8162941167e27
Kedarnaygaonkar <kedarn2006@gmail.com>
2026-03-25 21:24:50 +0530

    comparison_&_logical_operators

--- 02_reading.mongodb.js
+++ 02_reading.mongodb.js
@@ -1,4 +1,9 @@
 use('eCommerce');
 
 // db.products.find({"name" : "Wireless Mouse"});
-db.products.find({ category: "Electronics" })
+// db.products.find({ category: "Electronics" })
+
+// db.products.find({ price: { $gt: 1000 } })
+// db.products.find({ price: { $gte: 1000, $lte: 50000 } })
+
+db.products.find({ $or: [{ category: "Electronics" }, { stock: { $lt: 50 } }] })
